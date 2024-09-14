9ad8ba5a26bdcd194466c91c67f7a277418c621f
zaid kamil <xaidmetamorphos@gmail.com>
2024-09-14 16:44:49 +0530

    updated cart display

--- cart/templatetags/cart_tags.py
+++ cart/templatetags/cart_tags.py
@@ -1,13 +1,20 @@
 from django import template
+from cart.models import Cart, CartItem
 
 # create a object of Library class
 register = template.Library()
 
-
 # create a custom template tag
-@register.filter(takes_context=True)
+@register.simple_tag(takes_context=True)
 def cart_item_count(context):
     request = context['request']
-    count = request.session.get('cart', None)
-    print(count, 'items')
-    return count
+    if 'cart' in request.session:
+        return request.session['cart']
+    else:
+        # count items in cart
+        cart = Cart.objects.filter(user=request.user).first()
+        if cart:
+            no_of_items = cart.cartitem_set.count()
+            request.session['cart'] = no_of_items
+            return no_of_items
+        return 0
